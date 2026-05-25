774f52e265069dd3bef2d3ee5f316071613f2096
Chaoticmaple <harrybrown@rogers.com>
2026-05-25 19:01:37 -0400

    ch02 wip

--- chapters/ch02_number_bases/exercises.py
+++ chapters/ch02_number_bases/exercises.py
@@ -4,12 +4,33 @@
 
 def binary_to_decimal(string: str) -> str:
     """e.g. binary_to_decimal('11010') -> '26'"""
-    pass
+    total = 0
+    digit_position = len(string)-1
+
+    for char in string:
+        if (char == '1'):
+            total += 2 ** digit_position
+        digit_position -= 1
+    
+    text = str(total)
+    return text
 
 
 def hexadecimal_to_decimal(string: str) -> str:
     """e.g. hexadecimal_to_decimal('3B07F') -> '241791'"""
-    pass
+    total = 0
+    digit_position = len(string)-1
+    scalar_dict = {'1':1,'2':2,'3':3, '4':4,'5':5,'6':6,'7':7,'8':8,'9':9,
+                   'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
+
+    for char in string:
+        if (char != '0'):
+            total += (16 ** digit_position) * scalar_dict.get(char)
+        digit_position -= 1
+    
+    text = str(total)
+    
+    return text
 
 
 def decimal_to_binary(string: str) -> str:
