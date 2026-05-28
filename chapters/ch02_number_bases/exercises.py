da9dd9d0ef3b9cdc71df1401e60425aed2d1ed92
Harry Brown <harrybrown@rogers.com>
2026-05-27 23:46:12 -0400

    ch02 done

--- chapters/ch02_number_bases/exercises.py
+++ chapters/ch02_number_bases/exercises.py
@@ -67,9 +67,52 @@ def decimal_to_hexadecimal(string: str) -> str:
 
 def binary_to_hexadecimal(string: str) -> str:
     """Hint: compose binary_to_decimal and decimal_to_hexadecimal."""
-    pass
+    hex_chars = '0123456789ABCDEF'
+
+    length = len(string)
+    remainder = length % 4
+    if (remainder != 0):
+        zeroes = 4 - remainder
+        for i in range(zeroes):
+            string = '0' + string
+
+
+    text = ''
+    while (string):
+        if (len(string) == 0):
+            break
+        #take a slice
+        quad = string[:4]
+        string = string[4:]
+
+        bit_values = {'0': 0, '1': 1}
+        value = 0
+        for bit in quad:
+            value = value * 2 + bit_values[bit]
+        text += hex_chars[value]
+
+    return text
 
 
 def hexadecimal_to_binary(string: str) -> str:
     """Hint: compose hexadecimal_to_decimal and decimal_to_binary."""
-    pass
+    binary = '01'
+    hex_values = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,
+              'A':10,'B':11,'C':12,'D':13,'E':14,'F':15}
+
+    text = ''
+
+    if string == '0':
+        return '0'
+
+    while (string):
+        slice = string[:1]
+        string = string[1:]
+        digit = hex_values[slice]
+        chunk = ''
+        while (digit > 0):
+            chunk = str(digit % 2) + chunk
+            digit = digit // 2
+        text += chunk
+    return text
+
